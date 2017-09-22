c71cf744be065a0e16987f4810f325b7121c1694
goodcleanfun <robertfbatesjr@gmail.com>
2017-09-22 11:34:33 -0400

    Install Devise and generate Devise views

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,11 +1,11 @@
-import ReactOnRails from 'react-on-rails';
+import ReactOnRails from 'react-on-rails'
 
-import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
-import HelloWorldWithRailsContext from '../bundles/HelloWorld/components/HelloWorldWithRailsContext';
+import HelloWorld from '../bundles/HelloWorld/components/HelloWorld'
+import HelloWorldWithRailsContext from '../bundles/HelloWorld/components/HelloWorldWithRailsContext'
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 // Add all containers and components individually.
 ReactOnRails.register({
   HelloWorld,
   HelloWorldWithRailsContext
-});
+})
