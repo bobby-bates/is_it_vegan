d58e196d0ba2fdc5f69713cce01ceb740e9e8e19
goodcleanfun <robertfbatesjr@gmail.com>
2017-09-23 21:28:19 -0400

    Add coveralls gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,18 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'launchy'
+require "capybara/rails"
+require "valid_attribute"
+require 'rspec/rails'
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -53,6 +61,8 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
