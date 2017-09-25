a63cda90be624ae709f09320be6c41248f743d91
goodcleanfun <robertfbatesjr@gmail.com>
2017-09-25 16:43:29 -0400

    Add spec/support folder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -223,4 +223,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace('gem name')
+
+  # Attempted phantomjs fix:
+  # config.after :each do |example|
+  #   page.driver.restart if defined?(page.driver.restart)
+  # end
 end
